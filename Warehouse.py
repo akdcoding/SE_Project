ee4caf3684543d756cac7be17cca332a3493d8db
Kyaw Htet Paing WIn <kyawhtetpaing111@gmail.com>
2022-03-26 13:25:21 -0700

    bla

--- Warehouse.py
+++ Warehouse.py
@@ -1,14 +1,16 @@
 from uuid import uuid4
+
+
 # Inventory Lookup
 class Warehouse:
-    def __init__(self, id, location, capacity, items):
+    def __init__(self, location, capacity, items):
         self.id = uuid4()
         self.location = location
         self.capacity = capacity
         self.items = items
 
     def getProductList(self):
-        return (self.id, self.location, self.capacity, self.items)
+        return self.id, self.location, self.capacity, self.items
 
     def updateItemsList(self, items):
         self.items.append(items)
